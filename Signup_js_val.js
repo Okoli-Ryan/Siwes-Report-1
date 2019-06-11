6fe5d8ffc887c65595202e64e933a61c95d19c58
Okoli-Ryan <51544977+Okoli-Ryan@users.noreply.github.com>
2019-06-12 00:37:36 +0100

    Add files via upload

---
+++ Signup_js_val.js
@@ -0,0 +1,50 @@
+function ajaxRequest()
+{
+    try // Non-IE browser?
+    {
+        request = new XMLHttpRequest()
+    }
+    catch(e1)
+    {
+        try // IE 6+?
+        {
+            request = new ActiveXObject("Msxml2.XMLHTTP")
+        }
+        catch(e2)
+        {
+            try // IE 5?
+            {
+                request = new ActiveXObject("Microsoft.XMLHTTP")
+            }
+            catch(e3) // There is no Ajax support
+            {
+                request = false
+            }
+        }
+    }
+    return request
+}
+
+function check(str){
+
+    var request = ajaxRequest();
+
+    request.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200) {
+            if (this.responseText !== "already in use") {
+                document.getElementById("submit_button").disabled = false;
+                document.getElementById("username").style.borderBottomColor="#839bcb";
+
+            }
+
+            else {
+                document.getElementById("username").style.borderBottomColor="red";
+                document.getElementById("submit_button").disabled = true;
+            }
+        }
+    };
+    nocache = "&nocache=" + Math.random() * 1000000;
+    request.open("GET", "List_users.php?q=" + str + nocache, true);
+    request.send();
+
+}
